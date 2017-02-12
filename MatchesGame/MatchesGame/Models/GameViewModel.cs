032926660462cc17b8c631b33f5181a4a53a2aa0
Robertas <Robertas@DESKTOP-6HLCLKI>
2017-02-12 13:26:13 +0200

    Validation

--- MatchesGame/MatchesGame/Models/GameViewModel.cs
+++ MatchesGame/MatchesGame/Models/GameViewModel.cs
@@ -1,20 +1,18 @@
 ﻿using MatchesGame.Domain;
+using System.ComponentModel.DataAnnotations;
 
 namespace MatchesGame.Models
 {
     public class GameViewModel
     {
+        [Required(ErrorMessage = "Please select game type")]
         public string GameType { get; set; }
+        [Required(ErrorMessage = "Please enter first name")]
         public string PlayerOne { get; set; }
+        [Required(ErrorMessage = "Please enter second name")]
         public string PlayerTwo { get; set; }
 
-        //public static GameViewModel MapFrom(GameLog gamelog)
-        //{
-        //    var model = new GameViewModel
-        //    {
-                
-        //    };
-        //}
+        
     }
 
 
---
+++ MatchesGame/MatchesGame/Scripts/game-scripts/form.js
@@ -0,0 +1,23 @@
+﻿$(function () {
+    var dropDown = $("#GameType"),
+        playerTwo = $("#PlayerTwo"),
+        secondPlayer = $("#secondPlayer");
+
+    if (dropDown.val() === "1") {
+        playerTwo.val("Computer");
+        secondPlayer.hide();
+    }
+
+    dropDown.change(function () {
+        if (dropDown.val() === "1") {
+            playerTwo.val("Computer");
+            secondPlayer.hide();
+        }
+        else {
+            playerTwo.val("");
+            secondPlayer.show();
+        }
+
+        
+    });
+});
