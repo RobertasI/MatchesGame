c6988db2fb1854884855eb414844ffe52aa5f180
Robertas <Robertas@DESKTOP-6HLCLKI>
2017-02-12 22:47:55 +0200

    Two players game logic

--- MatchesGame/MatchesGame/Scripts/game-scripts/game-logic.js
+++ MatchesGame/MatchesGame/Scripts/game-scripts/game-logic.js
@@ -1,19 +1,25 @@
 ﻿$(function () {
-    var matchesCount = 21;
+    var matchesCount = 21,
+        firstPlayer = $("#FirstPlayer").text(),
+        secondPlayer = $("#SecondPlayer").text(),
+        currentPlayer = firstPlayer;
 
     $("#Take1").click(function () {
         updateMatches(1);
         updateButtons();
+        updatePlayer();
     });
 
     $("#Take2").click(function () {
         updateMatches(2);
         updateButtons();
+        updatePlayer();
     });
 
     $("#Take3").click(function () {
         updateMatches(3);
         updateButtons();
+        updatePlayer();
     });
 
     function updateMatches(takenMatches) {
@@ -22,6 +28,8 @@
         for (var i = 1; i <= matchesCount; i++) {
             $("#Matches").append("<span><img src='/Content/images/match.png' width='35' /></span> ");
         }
+        
+        
     };
     
     function updateButtons() {
@@ -33,5 +41,23 @@
             $("#Take2").hide();
         }
 
-    }
+        if (matchesCount < 1) {
+            $("#Take1").hide();
+        }
+    };
+
+    function updatePlayer() {
+        if (matchesCount < 1) {
+            $("#CurrentPlayer").text("Winner is: " + currentPlayer);
+        }
+        else {
+            if (currentPlayer === firstPlayer) {
+                currentPlayer = secondPlayer;
+            }
+            else {
+                currentPlayer = firstPlayer;
+            }
+            $("#CurrentPlayer").text(currentPlayer + " Turn");
+        }
+    };
 });
